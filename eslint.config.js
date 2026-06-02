8ba859fd98e8df39c3dbaa26f8c40c922b654e5f
rramires <ricardo@flexbr.com>
2026-06-01 22:28:22 -0300

    chore: add Prettier and ESLint configuration files

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,8 @@ import js from '@eslint/js'
 import globals from 'globals'
 import tseslint from 'typescript-eslint'
 import { defineConfig } from 'eslint/config'
+import eslintConfigPrettier from 'eslint-config-prettier'
+import simpleImportSort from 'eslint-plugin-simple-import-sort'
 
 export default defineConfig([
 	{ ignores: ['dist/*', 'node_modules/*'] },
@@ -16,10 +18,13 @@ export default defineConfig([
 	},
 	tseslint.configs.recommended,
 	{
+		plugins: { 'simple-import-sort': simpleImportSort },
 		rules: {
 			'prefer-const': 'warn',
 			'no-unused-vars': 'off',
 			'@typescript-eslint/no-unused-vars': 'warn',
+			'simple-import-sort/imports': 'error',
 		},
 	},
+	eslintConfigPrettier,
 ])
